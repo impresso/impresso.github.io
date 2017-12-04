658b056098800244429a2f5174b72b2d779887c3
Daniele Guido <gui.daniele@gmail.com>
2017-12-04 10:10:55 +0100

    fix floating side menu
    (toggle menu disabled)

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -445,6 +445,29 @@ window.ImpressoTimeline = function(options) {
     };
   }
 
+  function throttle(func, limit) {
+    var inThrottle,
+      lastFunc,
+      lastRan;
+    return function() {
+      var context = this,
+        args = arguments;
+      if (!inThrottle) {
+        func.apply(context, args);
+        lastRan = Date.now()
+        inThrottle = true;
+      } else {
+        clearTimeout(lastFunc)
+        lastFunc = setTimeout(function() {
+          if ((Date.now() - lastRan) >= limit) {
+            func.apply(context, args)
+            lastRan = Date.now()
+          }
+        }, limit - (Date.now() - lastRan))
+      }
+    };
+  };
+
   this.init();
 }
 
